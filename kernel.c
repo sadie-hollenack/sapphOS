fda6eb4bdd51429c42ee7af73618e2fd8b85e771
Sadie Hollenack <sadiehollenack@gmail.com>
2025-02-04 14:21:12 -0500

    Added a kernel panic feature

--- kernel.c
+++ kernel.c
@@ -45,6 +45,8 @@ void kernel_main(void) {
 	printf("\n\nHello %s\n", "World!");
 	printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
+	PANIC("booted!");
+	printf("unreachable");
 	// busy waits (this broke deepseeks ai LMAO)
 	for (;;){
 		// wait for interrupt
